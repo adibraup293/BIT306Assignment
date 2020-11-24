3a83cca399f1551fd09a71409bf2bab21b6e24cb
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2020-11-24 22:43:42 +0800

    able to enter data into database

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 app.post("/api/testkits", (req, res, next) => {
   const testKit = new TestKit({
     testKitName: req.body.testkitname,
-    testKitStock: req.body.testkitname
+    testKitStock: req.body.testkitstock
   })
 
   testKit.save().then(createdTestKit => {
